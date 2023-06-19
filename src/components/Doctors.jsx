135dc96f1f39089b76216a3e5745ebe0ae83320b
Achieve-software <vtcbcdtyr@gmail.com>
2023-06-19 15:12:00 +0300

    dr

---
+++ src/components/Doctors.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+const Doctors = ({ doctors }) => {
+  // console.log(doctors)
+  return (
+    <Container className="p-2">
+      <h3 className="display-6" style={{ color: "rgb(166,18,189)" }}>
+        Our Doctors
+      </h3>
+      <Row>
+        {doctors.map((dr) => (
+          <Col key={dr.id}>
+          <img src={dr.img} alt={dr.name} className="img-thumbnail" />
+          <h5>{dr.name}</h5>
+          <h6>{dr.dep}</h6>
+          </Col>
+        ))}
+        
+      </Row>
+    </Container>
+  );
+};
+
+export default Doctors;
