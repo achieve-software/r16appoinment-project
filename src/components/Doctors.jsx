5c7dffad20eed52218d57fb56b07ca3ef5972d4a
Achieve-software <vtcbcdtyr@gmail.com>
2023-06-19 16:07:40 +0300

    click

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,26 +1,29 @@
-import React from "react";
-import Row from "react-bootstrap/Row";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
+// import {Container} from "react-bootstrap"
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-const Doctors = ({ doctors }) => {
-  // console.log(doctors)
+import {AddModal} from "./AddModal";const Doctors = ({ doctors }) => {
+  const [show, setShow] = useState(false);  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);  const handleClick= ()=>{
+    handleShow()
+  } 
+   console.log(doctors);
   return (
     <Container className="p-2">
-      <h3 className="display-6" style={{ color: "rgb(166,18,189)" }}>
+      <h3 className="display-6 mb-3"  style={{ color: "rgb(166,18,189" }}>
         Our Doctors
-      </h3>
-      <Row>
-        {doctors.map((dr) => (
-          <Col key={dr.id} xs={6} sm={4} md={3}>
-          <img src={dr.img} alt={dr.name} className="img-thumbnail" />
-          <h5>{dr.name}</h5>
-          <h6>{dr.dep}</h6>
+      </h3>      <Row className="justify-content-center">
+        {doctors.map((dr, index) => (
+          <Col key={(index)} xs={6} sm={4} md={3}>
+            <img src={dr.img} alt={dr.name} className="img-thumbnail doctor-img"
+            onClick={handleClick}/>
+            <h5>{dr.name}</h5>
+            <h6>{dr.dep}</h6>
           </Col>
         ))}
-        
       </Row>
+      <AddModal show={show} handleClose={handleClose}/>
     </Container>
   );
-};
-
-export default Doctors;
+};export default Doctors;
