41b211d07b1e11d2d137dab9d84847777ea1575a
Achieve-software <vtcbcdtyr@gmail.com>
2023-06-20 13:46:33 +0300

    form-add

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,11 +1,31 @@
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';export function AddModal({show, handleClose}) {  return (
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';export function AddModal({show, handleClose}) {  return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -3,27 +3,36 @@ import Container from "react-bootstrap/Container";
 // import {Container} from "react-bootstrap"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {AddModal} from "./AddModal";const Doctors = ({ doctors }) => {
-  const [show, setShow] = useState(false);  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);  const handleClick= ()=>{
-    handleShow()
-  } 
-   console.log(doctors);
+import { AddModal } from "./AddModal";
+const Doctors = ({ doctors }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+    const handleClick = () => {
+    handleShow();
+  };
+  console.log(doctors);
   return (
     <Container className="p-2">
-      <h3 className="display-6 mb-3"  style={{ color: "rgb(166,18,189" }}>
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166,18,189" }}>
         Our Doctors
-      </h3>      <Row className="justify-content-center">
+      </h3>{" "}
+      <Row className="justify-content-center">
         {doctors.map((dr, index) => (
-          <Col key={(index)} xs={6} sm={4} md={3}>
-            <img src={dr.img} alt={dr.name} className="img-thumbnail doctor-img"
-            onClick={handleClick}/>
+          <Col key={index} xs={6} sm={4} md={3}>
+            <img
+              src={dr.img}
+              alt={dr.name}
+              className="img-thumbnail doctor-img"
+              onClick={handleClick}
+            />
             <h5>{dr.name}</h5>
             <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
-      <AddModal show={show} handleClose={handleClose}/>
+      <AddModal show={show} handleClose={handleClose} />
     </Container>
   );
-};export default Doctors;
+};
+export default Doctors;
