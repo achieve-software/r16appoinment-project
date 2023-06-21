f422bfd2a5c898377e904ea2a982b58f20c3e728
Achieve-software <vtcbcdtyr@gmail.com>
2023-06-21 14:19:01 +0300

    FaTimesCircle

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,6 +1,6 @@
 import { Row, Col } from "react-bootstrap"
 import  Container  from "react-bootstrap/Container"
-
+import { FaTimesCircle} from "react-icons/fa";
 
 const AppointmentList = ({appointments, setAppointments}) => {
   
@@ -13,14 +13,17 @@ const AppointmentList = ({appointments, setAppointments}) => {
 {appointments.length<1 && <img src="./img/appointment.jpg" alt="" width="70%"/>}
 
 {appointments.map((item) =>{
+  const{id,patient,consulted,doctor,day}=item
+
 return(
 <div>
   <Row>
 <Col>
 <h4>{item.patient}</h4>
 <h5>{item.doctor}</h5>
-<h6>{item.day}</h6>
-
+<h6>{new Date(day).toLocaleDateString()}</h6>
+<h6>{new Date(day).toLocaleTimeString()}</h6>
+<FaTimesCircle/>
 </Col>
 
 
